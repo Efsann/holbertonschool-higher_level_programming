a42fbbfc1e5608f12283fcd56e0e090eb3efdbf6
efsann <stiyevefsan@gmail.com>
2026-03-26 15:35:38 +0000

    Task 4: Fixed PEP8 whitespace errors

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    # Orijinal siyahının kopyasını yaradırıq
     copy_list = my_list[:]
-    
-    # İndeksi yoxlayırıq
     if idx < 0 or idx >= len(my_list):
         return copy_list
-    
-    # Dəyişikliyi kopyanın üzərində edirik
     copy_list[idx] = element
     return copy_list
