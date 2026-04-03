bb6ea211c634e24d88f8e4856ed17b77a6c99a89
efsann <stiyevefsan@gmail.com>
2026-04-03 16:36:42 +0000

    Fix pycodestyle E501 and W293

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
-"""
-Bu modul Rectangle (Düzbucaqlı) sinfini təyin edir.
-"""
+"""Defines a class Rectangle."""
 
 
 class Rectangle:
-    """
-    Düzbucaqlını təmsil edən sinif.
-
-    Atributlar:
-        number_of_instances (int): Mövcud obyektlərin sayı.
-        print_symbol (any): Çap üçün istifadə olunan simvol.
-    """
+    """Represent a rectangle."""
 
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """Yeni bir Rectangle yaradır."""
+        """Initialize a new Rectangle."""
         type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Eni geri qaytarır."""
+        """Get the width."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Eni təyin edir (validasiya ilə)."""
+        """Set the width."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -38,12 +30,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """Hündürlüyü geri qaytarır."""
+        """Get the height."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Hündürlüyü təyin edir (validasiya ilə)."""
+        """Set the height."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -51,18 +43,18 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Düzbucaqlının sahəsini qaytarır."""
+        """Return the area."""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Düzbucaqlının perimetrini qaytarır."""
+        """Return the perimeter."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """İki düzbucaqlıdan sahəsi böyük olanı qaytarır."""
+        """Compare two rectangles."""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
@@ -73,22 +65,21 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """Eni və hündürlüyü bərabər olan yeni bir obyekt (kvadrat) qaytarır."""
+        """Return a new square."""
         return cls(size, size)
 
     def __str__(self):
-        """Düzbucaqlını print_symbol ilə vizual təsvir edir."""
+        """Print the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        
         row = str(self.print_symbol) * self.__width
         return "\n".join([row for _ in range(self.__height)])
 
     def __repr__(self):
-        """Obyekti yenidən yaratmaq üçün lazım olan stringi qaytarır."""
+        """Return the representation."""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
-        """Obyekt silinəndə mesaj çap edir və sayğacı azaldır."""
+        """Delete the rectangle."""
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
