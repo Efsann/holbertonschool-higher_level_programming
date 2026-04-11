8bf7b4555a0fcba5349a4c3ef8f39194cae25a56
efsann <stiyevefsan@gmail.com>
2026-04-11 19:17:41 +0000

    Task 0: Lookup function to list object attributes

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Obyektin atributlarını axtaran modul.
+"""
+
+
+def lookup(obj):
+    """Obyektin mövcud atribut və metodlarının siyahısını qaytarır."""
+    return dir(obj)
