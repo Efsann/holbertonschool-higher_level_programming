0321ca69e500a464edc67fd1f1ef8b0b8f53e266
efsann <stiyevefsan@gmail.com>
2026-04-11 19:57:03 +0000

    Fix task 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -21,39 +21,32 @@ class Shape(ABC):
 
 
 class Circle(Shape):
-    """Circle class that inherits from Shape."""
+    """Circle class"""
 
     def __init__(self, radius):
-        """Initialize Circle with radius."""
         self.radius = radius
 
     def area(self):
-        """Calculate area of a circle."""
         return math.pi * (self.radius ** 2)
 
     def perimeter(self):
-        """Calculate perimeter of a circle."""
         return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
-    """Rectangle class that inherits from Shape."""
+    """Rectangle class"""
 
     def __init__(self, width, height):
-        """Initialize Rectangle with width and height."""
         self.width = width
         self.height = height
 
     def area(self):
-        """Calculate area of a rectangle."""
         return self.width * self.height
 
     def perimeter(self):
-        """Calculate perimeter of a rectangle."""
         return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
-    """Function that prints area and perimeter of a shape."""
     print("Area: {}".format(shape.area()))
     print("Perimeter: {}".format(shape.perimeter()))
