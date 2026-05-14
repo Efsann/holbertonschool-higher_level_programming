0c47b93666c035636ecce8502cfdb10a80e891ef
efsann <stiyevefsan@gmail.com>
2026-05-14 18:48:25 +0000

    Implement ABC and Duck Typing for shapes

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+Module for Shape, Circle, Rectangle and shape_info function
+"""
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    """Abstract class Shape"""
+
+    @abstractmethod
+    def area(self):
+        """Abstract method for area"""
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """Abstract method for perimeter"""
+        pass
+
+
+class Circle(Shape):
+    """Concrete class Circle"""
+
+    def __init__(self, radius):
+        """Constructor for Circle"""
+        self.radius = radius
+
+    def area(self):
+        """Calculates area of circle"""
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        """Calculates perimeter of circle"""
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    """Concrete class Rectangle"""
+
+    def __init__(self, width, height):
+        """Constructor for Rectangle"""
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """Calculates area of rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Calculates perimeter of rectangle"""
+        return 2 * (self.width + self.height)
+
+
+def shape_info(shape):
+    """Prints area and perimeter using duck typing"""
+    print("Area: {}".format(shape.area()))
+    print("Perimeter: {}".format(shape.perimeter()))
